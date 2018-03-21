9737b4b8f7c2f4f83be6fc75e5e3a815c75eef2d
Nguyen Vu Nhat Minh <nmmyprograms@gmail.com>
2018-03-21 13:57:38 +0700

    Updated contract code and testing code

--- ethereum/EtherMessage.sol
+++ ethereum/EtherMessage.sol
@@ -48,19 +48,28 @@ contract EtherMessage {
     }
     
     function sendMessage(address to, bytes message, bytes32 encryption) public onlyMember {
-        RelationshipType relation = relationships[msg.sender][to];
-        require(relation != RelationshipType.NoRelation);
-        require(relation != RelationshipType.Blocked);
+
+        // Whether A can send message to B depends on the relationship between B to A
+        require(relationships[to][msg.sender] == RelationshipType.Connected);
+
+        if (members[to].messageStartBlock == 0) {
+            members[to].messageStartBlock = block.number;
+        }
         
         emit messageSentEvent(msg.sender, to, message, encryption);
     }
     
-    function blockMessagesFrom(address a) public onlyMember {
-        relationships[msg.sender][a] = RelationshipType.Blocked;
+    // Blocking is one-way
+    function blockMessagesFrom(address from) public onlyMember {
+        require(relationships[msg.sender][from] == RelationshipType.Connected);
+
+        relationships[msg.sender][from] = RelationshipType.Blocked;
     }
     
-    function unblockMessagesFrom(address a) public onlyMember {
-        relationships[msg.sender][a] = RelationshipType.Connected;
+    function unblockMessagesFrom(address from) public onlyMember {
+        require(relationships[msg.sender][from] == RelationshipType.Blocked);
+
+        relationships[msg.sender][from] = RelationshipType.Connected;
     }
     
     function setName(bytes32 name) public onlyMember {
@@ -77,12 +86,29 @@ contract EtherMessage {
     }
     
     // CALL METHODS
-    function getContactList() public view returns (address[], uint[]) {
-        uint[] memory a = new uint[](2);
-        return (contactLists[msg.sender], a);
+    function getContactList() public view onlyMember
+        returns (address[] addresses, bytes32[] names, bytes32[] avatarUrls, 
+        bytes32[] pubkeyLefts, bytes32[] pubkeyRights, uint[] msgStartBlocks) {
+            addresses = contactLists[msg.sender];
+            uint count = addresses.length;
+
+            names = new bytes32[](count);
+            avatarUrls = new bytes32[](count);
+            pubkeyLefts = new bytes32[](count);
+            pubkeyRights = new bytes32[](count);
+            msgStartBlocks = new uint[](count);
+
+            for (uint i=0;i<count;i++) {
+                Member storage member = members[addresses[i]];
+                names[i] = member.name;
+                avatarUrls[i] = member.avatarUrl;
+                pubkeyLefts[i] = member.publicKeyLeft;
+                pubkeyRights[i] = member.publicKeyRight;
+                msgStartBlocks[i] = member.messageStartBlock;
+            }
     }
     
-    function getRelationWith(address a) public view returns (RelationshipType) {
+    function getRelationWith(address a) public view onlyMember returns (RelationshipType) {
         return relationships[msg.sender][a];
     }
 }
--- test/EtherMessage.test.js
+++ test/EtherMessage.test.js
@@ -20,115 +20,123 @@ beforeEach(async () => {
         .send({from: accounts[0], gas: '3000000'});
 });
 
-// describe('Joining and adding contacts', () => {
-//     it('deploy a contract', () => {
-//         assert.ok(contract.options.address);
-//     });
+describe('Joining and adding contacts', () => {
+    it('deploy a contract', () => {
+        assert.ok(contract.options.address);
+    });
 
-//     it('join success', async () => {
-//         var member = await contract.methods.members(accounts[0]).call();
-//         assert(!member.isMember);
+    it('join success', async () => {
+        var member = await contract.methods.members(accounts[0]).call();
+        assert(!member.isMember);
 
-//         await accountJoin(0);
+        await accountJoin(0);
         
-//         member = await contract.methods.members(accounts[0]).call();
-//         assert(member.isMember);
-//         var returnedPublicKeyStr = member.publicKeyLeft.substr(2) + member.publicKeyRight.substr(2);
+        member = await contract.methods.members(accounts[0]).call();
+        assert(member.isMember);
+        var returnedPublicKeyStr = member.publicKeyLeft.substr(2) + member.publicKeyRight.substr(2);
 
-//         var publicKey = utils.privateToPublic(testAccounts[0].secretKey);
-//         assert.equal(publicKey.toString('hex'), returnedPublicKeyStr);
-//     });
+        var publicKey = utils.privateToPublic(testAccounts[0].secretKey);
+        assert.equal(publicKey.toString('hex'), returnedPublicKeyStr);
+    });
 
-//     it('add contact without joining', async () => {
-//         try {
-//             await addContact(0, 1);
-//             assert(false);
-//         } catch (err) {
-//             assert(err);
-//         }
-//     });
+    it('add contact without joining', async () => {
+        var hasError = false;
+        try {
+            await addContact(0, 1);
+        } catch (err) {
+            if (err) {
+                hasError = true;
+            }
+        }
+        assert(hasError);
+    });
 
-//     it('add contact after joining', async () => {
-//         await accountJoin(0);
-//         await addContact(0, 1);
-//         var relationship = await contract.methods.getRelationWith(accounts[1]).call({
-//             from: accounts[0]
-//         });
-//         assert.equal(1, relationship);
+    it('add contact after joining', async () => {
+        await accountJoin(0);
+        await addContact(0, 1);
+        var relationship = await contract.methods.getRelationWith(accounts[1]).call({
+            from: accounts[0]
+        });
+        assert.equal(1, relationship);
 
-//         var relationshipWithAccount2 = await contract.methods.getRelationWith(accounts[1]).call({
-//             from: accounts[2]
-//         });
-//         assert.equal(0, relationshipWithAccount2);
-//     });
+        await accountJoin(2);
+        var relationshipWithAccount2 = await contract.methods.getRelationWith(accounts[1]).call({
+            from: accounts[2]
+        });
+        assert.equal(0, relationshipWithAccount2);
+    });
 
-//     it('confirm contact request failed if there is no request', async () => {
-//         accountJoin(0);
-//         try {
-//             await acceptContactRequest(0, 1);
-//             assert(false);
-//         } catch (err) {
-//             assert(err);
-//         }
-//         await addContact(0, 1);
-//         try {
-//             await acceptContactRequest(0, 1);
-//             assert(false);
-//         } catch (err) {
-//             assert(err);
-//         }
-//     });
+    it('confirm contact request failed if there is no request', async () => {
+        accountJoin(0);
+        var hasError = false;
+        try {
+            await acceptContactRequest(0, 1);
+        } catch (err) {
+            if (err) hasError = true;
+        }
+        assert(hasError);
 
-//     it('confirm contact request success', async () => {
-//         await accountJoin(0);
-//         await addContact(0, 1);
+        hasError = false;
+        await addContact(0, 1);
+        try {
+            await acceptContactRequest(0, 1);
+        } catch (err) {
+            if (err) hasError = true;
+        }
+        assert(hasError);
+    });
 
-//         // Account 1 hasn't joined, so, he shouldn't be able to confirm contact request
-//         try {
-//             await acceptContactRequest(1, 0);
-//             assert(false);
-//         } catch (err) {
-//             assert(err);
-//         }
+    it('confirm contact request success', async () => {
+        await accountJoin(0);
+        await addContact(0, 1);
 
-//         await accountJoin(1);
-//         await acceptContactRequest(1, 0);
-//         var relationship = await contract.methods.getRelationWith(accounts[1]).call({
-//             from: accounts[0]
-//         });
-//         assert.equal(2, relationship);
-//     });
+        // Account 1 hasn't joined, so, he shouldn't be able to confirm contact request
+        var hasError = false;
+        try {
+            await acceptContactRequest(1, 0);
+        } catch (err) {
+            if (err) hasError = true;
+        }
+        assert(hasError);
 
-//     it('test contact list', async () => {
-//         await accountJoin(0);
-//         await accountJoin(1);
-//         await accountJoin(2);
-//         await accountJoin(3);
+        await accountJoin(1);
+        await acceptContactRequest(1, 0);
+        var relationship = await contract.methods.getRelationWith(accounts[1]).call({
+            from: accounts[0]
+        });
+        assert.equal(2, relationship);
+    });
 
-//         await addContact(0, 1);
-//         await addContact(1, 0);
-//         await addContact(0, 2);
-//         await addContact(0, 3);
+    it('test contact list', async () => {
+        await accountJoin(0);
+        await accountJoin(1);
+        await accountJoin(2);
+        await accountJoin(3);
 
-//         await acceptContactRequest(1, 0);
-//         await acceptContactRequest(2, 0);
-//         await acceptContactRequest(3, 0);
+        await addContact(0, 1);
+        await addContact(1, 0);
+        await addContact(0, 2);
+        await addContact(0, 3);
 
-//         var contactList0 = await contract.methods.getContactList().call({
-//             from: accounts[0]
-//         });
-//         assert.equal(contactList0[0].length, 3);
-//         assert.equal(contactList0[0][2], accounts[3]);
+        await acceptContactRequest(1, 0);
+        await acceptContactRequest(2, 0);
+        await acceptContactRequest(3, 0);
 
-//         var contactList1 = await contract.methods.getContactList().call({
-//             from: accounts[1]
-//         });
-//         assert.equal(contactList1[0][0], accounts[0]);
-//     });
-// });
+        var contactList0 = await contract.methods.getContactList().call({
+            from: accounts[0]
+        });
+        assert.equal(contactList0['addresses'].length, 3);
+        assert.equal(contactList0['addresses'][2], accounts[3]);
+
+        var contactList1 = await contract.methods.getContactList().call({
+            from: accounts[1]
+        });
+        assert.equal(contactList1['addresses'][0], accounts[0]);
+    });
+});
 
 describe('Sending message', () => {
-    it('Send raw messages', async () => {
+    it('Can send raw messages', async () => {
         await accountJoin(0);
         await accountJoin(1);
         await addContact(0,1);
@@ -152,6 +160,206 @@ describe('Sending message', () => {
             assert(messages[i], hexToString(returnedMessage));
         }
     });
+
+    it('Can\'t send message to non member', async () => {
+        await accountJoin(0);
+
+        var hasError = false;
+        try {
+            await contract.methods.sendMessage(accounts[1], stringToHex('test message'), stringToHex('no')).send({
+                from: accounts[0],
+                gas: '3000000'
+            });
+        } catch (err) {
+            if (err) {
+                hasError = true;
+            }
+        }
+        assert(hasError);
+    });
+
+    it('Can\'t send message to member before contact request accepted', async () => {
+        await accountJoin(0);
+        await accountJoin(1);
+        await addContact(0,1);
+
+        var hasError = false;
+        try {
+            await contract.methods.sendMessage(accounts[1], stringToHex('test message'), stringToHex('no')).send({
+                from: accounts[0],
+                gas: '3000000'
+            });
+        } catch (err) {
+            if (err) {
+                hasError = true;
+            }
+        }
+        assert(hasError);
+    });
+
+    it('Can send encrypted message', async () => {
+        await accountJoin(0);
+        await accountJoin(1);
+        await addContact(0,1);
+        await acceptContactRequest(1, 0);
+    });
+
+    it('Encrypt decrypt message', () => {
+        var pubkeyUser0 = '04' + utils.privateToPublic(testAccounts[0].secretKey).toString('hex');
+        var pubkeyUser1 = '04' + utils.privateToPublic(testAccounts[1].secretKey).toString('hex');
+        var secret0 = utils.computeSecret(testAccounts[0].secretKey, Buffer.from(pubkeyUser1, 'hex'));
+        var secret1 = utils.computeSecret(testAccounts[1].secretKey, Buffer.from(pubkeyUser0, 'hex'));
+
+        assert.equal(secret0.toString('hex'), secret1.toString('hex'));
+
+        var message = 'this is just a test message';
+        var encrypted = utils.encrypt(message, secret0);
+        var decrypted = utils.decrypt(encrypted, secret1);
+
+        assert.equal(message, decrypted.toString('hex'));
+    });
+
+    it('Can send encrypted messages', async () => {
+        await accountJoin(0);
+        await accountJoin(1);
+        await addContact(0,1);
+        await acceptContactRequest(1, 0);
+
+        var pubkeyUser0 = '04' + utils.privateToPublic(testAccounts[0].secretKey).toString('hex');
+        var pubkeyUser1 = '04' + utils.privateToPublic(testAccounts[1].secretKey).toString('hex');
+        var secret0 = utils.computeSecret(testAccounts[0].secretKey, Buffer.from(pubkeyUser1, 'hex'));
+        var secret1 = utils.computeSecret(testAccounts[1].secretKey, Buffer.from(pubkeyUser0, 'hex'));
+
+        var messages = ['Đây là một tin utf8', 'hello account 1', 'testing', 'how about a very long message how about a very long message how about a very long message how about a very long message how about a very long message how about a very long message how about a very long message how about a very long message how about a very long message how about a very long message how about a very long message'];
+        
+        for (var i=0;i<messages.length;i++) {
+            await contract.methods.sendMessage(accounts[1], 
+                '0x' + utils.encrypt(messages[i], secret0).toString('hex'), 
+                stringToHex('aes256')).send({
+                from: accounts[0],
+                gas: '3000000'
+            });
+        }
+
+        var member1 = await contract.methods.members(accounts[1]).call({
+            from: accounts[1]
+        });
+        assert(member1.messageStartBlock > 0);
+
+        var messageEvents = await contract.getPastEvents('messageSentEvent',{
+            filter: {},
+            fromBlock: member1.messageStartBlock
+        });
+
+        for (var i=0;i<messages.length;i++) {
+            var returnedMessage = messageEvents[i].returnValues.message;
+            var decryptedMessage = utils.decrypt(Buffer.from(returnedMessage.substr(2), 'hex'), secret1);
+            assert(messages[i], decryptedMessage.toString('ascii'));
+        }
+    });
+});
+
+describe('Blocking users', () => {
+    it('Block message from a user', async () => {
+        await accountJoin(0);
+        await accountJoin(1);
+        await addContact(0,1);
+        await acceptContactRequest(1, 0);
+        await blockMessagesFromAccountToAccount(1,0);
+
+        var hasError = false;
+        try {
+            await contract.methods.sendMessage(accounts[1], stringToHex('test message'), stringToHex('no')).send({
+                from: accounts[0],
+                gas: '3000000'
+            });
+        } catch (err) {
+            if (err) {
+                hasError = true;
+            }
+        }
+        assert(hasError);
+
+        // User 1 can send message to user 0
+        await contract.methods.sendMessage(accounts[0], stringToHex('test message'), stringToHex('no')).send({
+            from: accounts[1],
+            gas: '3000000'
+        });
+        assert(true);
+    });
+
+    it('block a not connected member', async() => {
+        var hasError = false;
+        try {
+            await blockMessagesFromAccountToAccount(0, 1);
+        } catch (err) {
+            if (err) {
+                hasError = true;
+            }
+        }
+        assert(hasError);
+
+        await accountJoin(0);
+        await accountJoin(1);
+        await addContact(0,1);
+
+        var hasError = false;
+        try {
+            await blockMessagesFromAccountToAccount(0, 1);
+        } catch (err) {
+            if (err) {
+                hasError = true;
+            }
+        }
+        assert(hasError);
+
+        await acceptContactRequest(1, 0);
+
+        var hasError = false;
+        try {
+            await blockMessagesFromAccountToAccount(0, 1);
+            await blockMessagesFromAccountToAccount(1, 0);
+        } catch (err) {
+            if (err) {
+                hasError = true;
+            }
+        }
+        assert(!hasError);
+    });
+
+    it('Unblock messages', async () => {
+        await accountJoin(0);
+        await accountJoin(1);
+        await addContact(0,1);
+        await acceptContactRequest(1, 0);
+        await blockMessagesFromAccountToAccount(1,0);
+
+        var hasError = false;
+        try {
+            await unblockMessagesFromAccountToAccount(0, 1);
+        } catch (err) {
+            if (err) {
+                hasError = true;
+            }
+        }
+        assert(hasError);
+
+        
+        hasError = false;
+        try {
+            await unblockMessagesFromAccountToAccount(1, 0);
+        } catch (err) {
+            if (err) {
+                hasError = true;
+            }
+        }
+        assert(!hasError);
+
+        var relationship = await contract.methods.getRelationWith(accounts[0]).call({
+            from: accounts[1]
+        });
+        assert.equal(2, relationship);
+    })
 });
 
 stringToHex = (text) => {
@@ -182,6 +390,20 @@ addContact = async (fromAccountIndex, toAccountIndex) => {
 
 acceptContactRequest = async (fromAccountIndex, toAccountIndex) => {
     await contract.methods.acceptContactRequest(accounts[toAccountIndex]).send({
+        from: accounts[fromAccountIndex],
+        gas: '3000000'
+    });
+}
+
+blockMessagesFromAccountToAccount = async (fromAccountIndex, toAccountIndex) => {
+    await contract.methods.blockMessagesFrom(accounts[toAccountIndex]).send({
+        from: accounts[fromAccountIndex],
+        gas: '3000000'
+    });
+}
+
+unblockMessagesFromAccountToAccount = async (fromAccountIndex, toAccountIndex) => {
+    await contract.methods.unblockMessagesFrom(accounts[toAccountIndex]).send({
         from: accounts[fromAccountIndex],
         gas: '3000000'
     });
